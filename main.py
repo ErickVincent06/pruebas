6772854ee1656eaf28d27801c0454c17ae06d67f
YoPusTons <erick.vincent1@outlook.com>
2024-09-03 08:05:31 -0600

    Unimportant changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import spotipy
-from spotipy import oauth2
+from spotipy.oauth2 import SpotifyAuthBase
 import os
 
 CLIENT_ID = os.environ.get('SP_ID_APP')
 CLIENT_SECRET = os.environ.get('SP_PSW')
-
+REDIRECT_URI = 'http://localhost:8000/callback'
